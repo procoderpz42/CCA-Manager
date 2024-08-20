317bd6fa2e623c4c3077dbf51a229ecae441b4c4
procoderpz42 <franstinjosephfrancis@gmail.com>
2024-08-20 23:03:40 +0800

    modified:   app.py

--- app.py
+++ app.py
@@ -242,5 +242,5 @@ def get_session():
         return redirect(url_for('cca_session'))
         
 if __name__=="__main__":
-    app.run(debug=True, port=0.0.0.0)
+    app.run(debug=True, port=10000)
 
